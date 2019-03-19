4792b04e55a51555544672756c7bdc0c91fc955a
momanyisam <momanyisamuel48@gmail.com>
2019-03-19 14:49:45 +0300

    added three components

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchBar.css'
+
+const sortByOptions = {
+    "Best Match" : "best_match",
+    "Highest Rated" : "rating",
+    "Most Viewed": "review_count"
+}
+
+class SearchBar extends React.Component {
+    renderSortByOptions () {
+        
+    }
+}
