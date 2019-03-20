2725c6816281b83bbba38ee60e4a2c74b3d4a6a2
momanyisam <momanyisamuel48@gmail.com>
2019-03-20 12:16:33 +0300

    added three components

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Flea Market</h1>
         <SearchBar />
         <BusinessList />
       </div>
